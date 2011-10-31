390438884a0cb094ae04cea9c25bae4778b735be
Robin Harms Oredsson <robin@betahaus.net>
2011-10-31 13:27:36 +0100

    Using patcher method from voteit.core

--- voteit/dutt/__init__.py
+++ voteit/dutt/__init__.py
@@ -5,9 +5,17 @@ DuttMF = TranslationStringFactory('voteit.dutt')
 
 
 def includeme(config):
+    #Include poll plugin
     from voteit.core.models.interfaces import IPoll
     from voteit.core.models.interfaces import IPollPlugin
     from voteit.dutt.plugin import DuttPoll
-    
     config.registry.registerAdapter(DuttPoll, (IPoll,), IPollPlugin, DuttPoll.name)
-   # config.add_translation_dirs('voteit.dutt:locale/')
+
+    #Include current dir in search path for deform templates
+    from voteit.core.patches import append_search_path
+    from pkg_resources import resource_filename
+    CURRENT_PATH = resource_filename('voteit.dutt', '')
+    append_search_path(CURRENT_PATH)
+
+    #Include translations
+    #config.add_translation_dirs('voteit.dutt:locale/')
