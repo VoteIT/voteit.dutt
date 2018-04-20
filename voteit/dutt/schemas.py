1e6be61b42be6ef30a01caf1785282f9dabae2e2
Robin Harms Oredsson <robin@betahaus.net>
2018-04-20 14:06:31 +0200

    Cosmetic change

--- voteit/dutt/schemas.py
+++ voteit/dutt/schemas.py
@@ -11,6 +11,7 @@ def deferred_proposal_title(node, kw):
         return _(u"Mark the proposals you wish to vote for")
     return _(u"You can't change your vote now.")
 
+
 @colander.deferred
 def deferred_proposal_description(node, kw):
     context = kw['context']
@@ -18,29 +19,33 @@ def deferred_proposal_description(node, kw):
     min_choices = context.poll_settings.get('min', 0)
     if min_choices:
         return _("proposal_description_min",
-                 default = "Check at least ${min} and at most ${max} proposal(s).",
-                 mapping = {'min': min_choices, 'max': max_choices})
+                 default="Check at least ${min} and at most ${max} proposal(s).",
+                 mapping={'min': min_choices, 'max': max_choices})
     return _("proposal_description_without_min",
-             default = "Check at most ${max} proposal(s).",
-             mapping = {'max': max_choices})
+             default="Check at most ${max} proposal(s).",
+             mapping={'max': max_choices})
+
 
 @colander.deferred
 def deferred_proposal_widget(node, kw):
     context = kw['context']
-    #Choices should be something iterable with the contents [(UID for proposal, Title of proposal), <etc...>, ]
+    # Choices should be something iterable with the contents [(UID for proposal, Title of proposal), <etc...>, ]
     choices = []
     proposals = context.get_proposal_objects()
     for prop in proposals:
         choices.append((prop.uid, prop.title))
     max_choices = context.poll_settings.get('max', 0)
     if max_choices >= len(choices):
-        #Disable info text if max is more or the same as the maximum available
+        # Disable info text if max is more or the same as the maximum available
         max_choices = 0
     min_choices = context.poll_settings.get('min', 0)
-    return deform.widget.CheckboxChoiceWidget(values = choices,
-                                              css_class = 'dutt_proposals',
-                                              max = max_choices,
-                                              min = min_choices,)
+    return deform.widget.CheckboxChoiceWidget(
+        values=choices,
+        css_class='dutt_proposals',
+        max=max_choices,
+        min=min_choices,
+    )
+
 
 @colander.deferred
 def deferred_dutts_validator(node, kw):
@@ -49,14 +54,17 @@ def deferred_dutts_validator(node, kw):
 
 
 class DuttSchema(colander.Schema):
-    widget = deform.widget.FormWidget(template = 'form_modal',
-                                      readonly_template = 'readonly/form_modal')
+    widget = deform.widget.FormWidget(
+        template='form_modal',
+        readonly_template='readonly/form_modal'
+    )
     proposals = colander.SchemaNode(
-                    colander.List(),
-                    widget = deferred_proposal_widget,
-                    title = deferred_proposal_title,
-                    description = deferred_proposal_description,
-                    validator = deferred_dutts_validator)
+        colander.List(),
+        widget=deferred_proposal_widget,
+        title=deferred_proposal_title,
+        description=deferred_proposal_description,
+        validator=deferred_dutts_validator
+    )
 
 
 class DuttValidator(object):
@@ -72,24 +80,32 @@ class DuttValidator(object):
         assert isinstance(min_choices, int)
         if max_choices:
             if len(value) > max_choices:
-                raise colander.Invalid(node, _(u"too_many_selected_error",
-                                       default = u"You can only select a maximum of ${max}.",
-                                       mapping = {'max': max_choices}))
+                raise colander.Invalid(
+                    node, _(u"too_many_selected_error",
+                            default=u"You can only select a maximum of ${max}.",
+                            mapping={'max': max_choices})
+                )
         if min_choices:
             if len(value) < min_choices:
-                raise colander.Invalid(node, _(u"too_few_selected_error",
-                                       default = u"You must select at least ${min}.",
-                                       mapping = {'min': min_choices}))
+                raise colander.Invalid(
+                    node, _(u"too_few_selected_error",
+                            default=u"You must select at least ${min}.",
+                            mapping={'min': min_choices})
+                )
 
 
 class DuttSettingsSchema(colander.Schema):
-    max = colander.SchemaNode(colander.Int(),
-                              title = _(u"Maximum dutts"),
-                              description = _(u"A '0' disables this setting."),
-                              default = 0,
-                              missing = 0)
-    min = colander.SchemaNode(colander.Int(),
-                              title = _(u"Minimum dutts"),
-                              description = _(u"A '0' disables this setting."),
-                              default = 0,
-                              missing = 0)
+    max = colander.SchemaNode(
+        colander.Int(),
+        title=_(u"Maximum dutts"),
+        description=_(u"A '0' disables this setting."),
+        default=0,
+        missing=0
+    )
+    min = colander.SchemaNode(
+        colander.Int(),
+        title=_(u"Minimum dutts"),
+        description=_(u"A '0' disables this setting."),
+        default=0,
+        missing=0
+    )
